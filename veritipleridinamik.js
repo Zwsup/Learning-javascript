a95fa65945642d0f45c072ba77d023b84a625b84
Kemal Arda Girgin <kemalardagirgin34611@gmail.com>
2026-03-18 22:16:33 +0300

    dinamik veri tipleri öğrenildi

---
+++ veritipleridinamik.js
@@ -0,0 +1,11 @@
+// javascript değişkenleri dinamik veritipine sahiptir. atanan değere göre veri tipi otomatik değişir.
+
+var degisken = 5; // Number
+
+degisken = 3.3; // Number
+
+degisken = "metin"; // String
+
+degisken = true; // Booelan
+
+degisken = null // Null
