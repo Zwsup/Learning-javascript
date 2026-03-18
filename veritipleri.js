bef6bb47475f2afd599d75c8c36d191ce4ff8ca3
Kemal Arda Girgin <kemalardagirgin34611@gmail.com>
2026-03-18 22:03:21 +0300

    değişken veri tipleri öğrenildi

---
+++ veritipleri.js
@@ -0,0 +1,17 @@
+//Değişken veri tipleri
+
+//Number: Tam sayılar veya ondalıklı sayılar
+
+var x=5;
+
+//String: Sözel veri
+
+var ad="arda";
+
+//Boolean: Mantıksal veri tipi değişkene true veya false değerini atarsak booelan tipine dönüşür
+
+//Array: Diziler
+
+//undefined: Tanımlanmamış ise undefined olarak gözükür
+
+//Null: Boş veya değer yok.
