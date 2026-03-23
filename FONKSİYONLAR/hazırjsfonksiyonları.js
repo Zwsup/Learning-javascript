3fb2c3920c9d85fdd130497cf49563d87b57835e
Kemal Arda Girgin <kemalardagirgin34611@gmail.com>
2026-03-24 00:58:49 +0300

    Hazır fonksiyonlar hakkında bilgi verildi

---
+++ FONKSİYONLAR/hazırjsfonksiyonları.js
@@ -0,0 +1,12 @@
+//Hazır Javascript Fonksiyonları
+
+//Javascript içinde gömülü olarak kullanıma hazır yüzlerce fonksiyon gelir.
+
+//Bu fonksiyonlar sayesinde sayısal, sözel veya tarihsel işlemler çok daha kolay yapılabilir.
+
+//Math
+//Number
+//String
+//Date
+//Array
+//Boolean
